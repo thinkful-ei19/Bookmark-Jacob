fc75da7a93ef49727379ae2906d333a9701174bf
JacobAZimmerman <ijacobzim@gmail.com>
2018-03-01 11:47:44 -0800

    created client side data store functionalities

--- scripts/index.js
+++ scripts/index.js
@@ -3,6 +3,6 @@
 $(function(){
   api.getBookmarks((bookmarks) => {
     bookmarks.forEach((bookmark) => store.addBookmark(bookmark));
-    bookmark.render();
+    // bookmark.render();
   });
 });
--- scripts/store.js
+++ scripts/store.js
@@ -1,15 +1,49 @@
 'use strict';
 const store = (function() {
-  const state = {
-    bookmarks:[],
-    filter:false
-  };
+
+  function findById(id) {
+    return this.bookmarks.find(bookmark => bookmark.id === id);
+  }
+
+  function findAndDelete(id) {
+    this.bookmarks = this.bookmarks.filter(bookmark => bookmark.id !== id);
+  }
+
+  function findAndUpdate(id, newData) {
+    Object.assign(findById(id), newData);
+  }
 
   function addBookmark(bookmark) {
-    state.bookmarks.push(bookmark);
+    this.bookmarks.push(decorateBookmark(bookmark));
+  }
+
+  function decorateBookmark(bookmark) {
+    bookmark.expand = false;
+    return bookmark;
+  }
+
+  function decorateBookmarks(bookmarks) {
+    return bookmarks.forEach(bookmark => decorateBookmark(bookmark));
+  }
+
+  function setFilter(minStars) {
+    this.filter = minStars;
+  }
+
+  function setFilterOff() {
+    this.filter = false;
   }
 
   return {
-    addBookmark, state
+    addBookmark,
+    bookmarks: [],
+    filter: false,
+    findById,
+    findAndDelete,
+    findAndUpdate,
+    decorateBookmarks,
+    decorateBookmark,
+    setFilter,
+    setFilterOff
   };
 })();
